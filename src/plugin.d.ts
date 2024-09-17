518dd0f96be039b919244a6316d988a22de37bda
Marc Addeo <marcaddeo@gmail.com>
2024-09-17 09:19:36 -0400

    Add .editorconfig

--- src/plugin.d.ts
+++ src/plugin.d.ts
@@ -1,25 +1,25 @@
 declare module "main" {
-    export function hank();
-    export function get_metadata(): I64;
-    export function install();
-    export function initialize();
-    export function handle_message();
-    export function handle_command();
+  export function hank();
+  export function get_metadata(): I64;
+  export function install();
+  export function initialize();
+  export function handle_message();
+  export function handle_command();
 }
 
 declare module "extism:host" {
-    interface user {
-        send_message(input: I64): I64;
-        react(input: I64): I64;
-        db_query(input: I64): I64;
-        cron(input: I64): I64;
-    }
+  interface user {
+    send_message(input: I64): I64;
+    react(input: I64): I64;
+    db_query(input: I64): I64;
+    cron(input: I64): I64;
+  }
 }
 
 declare namespace NodeJS {
-    export interface Global {
-        hank: any;
-    }
+  export interface Global {
+    hank: any;
+  }
 }
 
 declare var hank: any;
