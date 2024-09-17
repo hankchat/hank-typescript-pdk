0a66a423d91c952c7b7f2bbde3cd25cc47fb0368
Marc Addeo <marcaddeo@gmail.com>
2024-09-17 08:27:51 -0400

    Fix packaging issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["cjs"],
+	entry: ["src/*.ts"],
+	format: ["cjs", "esm"],
 	dts: true,
 	splitting: false,
 	sourcemap: true,
