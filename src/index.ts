62804c5e28b63eef8dc7eb1c2321f96e841a60cb
Marc Addeo <marcaddeo@gmail.com>
2024-09-18 19:28:35 -0400

    Export Hank

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ class HankRpc implements Rpc {
   }
 }
 
-class Hank {
+export class Hank {
   protected client: HankClientImpl;
   protected metadata: Metadata;
   protected messageHandler: Function | undefined;
